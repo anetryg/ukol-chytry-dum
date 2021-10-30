d44df6fd787f3ceeac18dc7bcd6162997a2f651d
Aneta Ryglova <aneta.ryglova@asseco-ce.com>
2021-10-30 17:28:44 +0200

    add energy description

--- src/components/Energy/index.jsx
+++ src/components/Energy/index.jsx
@@ -9,13 +9,17 @@ const Energy = ({electricity, water}) =>{
             <div className="energy__source">
                 <img className="energy__icon" src={electricityImage} />
                 <div className="energy__consumption">
-                    <p className="energy__value">{electricity} kW</p>
+                    <p className="energy__value">
+                        <div className="energy__description">Elektřina</div>{electricity} kW
+                    </p>
                 </div>
             </div>
             <div className="energy__source">
                 <img className="energy__icon" src={waterImage} />
                 <div className="energy__consumption">
-                    <p className="energy__value">{water} m<sup>3</sup></p>
+                    <p className="energy__value">
+                        <div className="energy__description">Voda</div>{water} m<sup>3</sup>
+                    </p>
                 </div>
             </div>
         </div>
